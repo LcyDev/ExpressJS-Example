2d5d7ac5626eb72eef0b1c97510beb58c76212ed
Lucyy_ <72966048+LcyDev@users.noreply.github.com>
2024-06-26 02:59:33 -0300

    Refactor product model: added validation and constraints

--- src/models/Product.js
+++ src/models/Product.js
@@ -1,9 +1,30 @@
-import mongoose from 'mongoose';
+import mongoose, { Schema } from 'mongoose';
 
-const productSchema = new mongoose.Schema({
-    name: String,
-    price: Number,
-    description: String
+const productSchema = new Schema({
+    name: {
+        type: String,
+        required: [true, 'Name is required'],
+        trim: true,
+    },
+    price: {
+        type: Number,
+        required: [true, 'Price is required'],
+        min: 0,
+    },
+    description: {
+        type: String,
+        trim: true,
+    },
+});
+
+productSchema.set('toJSON', {
+    virtuals: true,
+/*
+    versionKey: false, // Prevent the _v field
+    transform: (doc, ret) => {
+        delete ret._id; // Delete the _id field
+    },
+*/
 });
 
 const Product = mongoose.model('Product', productSchema);
