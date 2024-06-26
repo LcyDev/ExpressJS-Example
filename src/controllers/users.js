e78ff84caad76190938a203e56e6e74332d0048e
Lucyy_ <72966048+LcyDev@users.noreply.github.com>
2024-06-26 17:53:32 -0300

    Refactor controllers for users
    Simplified the code by removing unnecessary variables and using more concise syntax.
    
    Improved error handling by using try-catch blocks.
    
    Used arrow functions to define the controller functions, which makes the code more concise and easier to read.

--- src/controllers/users.js
+++ src/controllers/users.js
@@ -2,116 +2,103 @@ import User from '../models/User.js';
 import errorHandler from '../utils/errorHandler.js';
 import mongoose from 'mongoose';
 
-async function getUsers(req, res) {
+const getUsers = async (req, res) => {
     console.log('(GET) Getting all users...');
-    User.find()
-    .then(users => {
-        console.log(`(200) Found ${users.length} users`);
+    try {
+        const users = await User.find();
         res.status(200).json(users);
-    })
-    .catch(error => {
+        console.log(`(200) Found ${users.length} users`);
+    } catch (error) {
         errorHandler(res, error, 'Error fetching users');
-    });
-}
-
-async function createUser(req, res) {
-    console.log('(POST) Creating new user...', req.body);
-    const { name, age, email } = req.body;
-    const user = new User({ name, age, email });
-    await user.save()
-    .then(savedUser => {
-        console.log(`(201) User created: ${savedUser.name}`);
-        res.status(201).json(savedUser);
-    })
-    .catch(error => {
-        errorHandler(res, error, 'Error creating user');
-    });
-}
-
-async function createMultipleUsers(req, res) {
-    console.log('(POST) Creating multiple users...', req.body);
-    const usersToCreate = req.body;
-    const createdUsers = usersToCreate.map(user => {
-        return new User(user).save();
-    });
-    await Promise.all(createdUsers)
-    .then(createdUsers => {
-        console.log(`(201) ${createdUsers.length} users created`);
-        res.status(201).json(createdUsers);
-    })
-    .catch(error => {
-        errorHandler(res, error, 'Error creating multiple users');
-    });
-}
-
-async function updateUser(req, res) {
-    console.log('(PUT) Updating user...', req.body);
-    const id = req.params.id;
-    if (!mongoose.isValidObjectId(id)) {
-        errorHandler(res, new Error('Invalid ObjectId'), undefined, 400);
-        return;
     }
-    User.findByIdAndUpdate(id, req.body, { new: true })
-    .then(user => {
+};
+
+const createUser = async (req, res) => {
+    console.log('(POST) Creating new user...', req.body);
+    try {
+        const { name, age, email } = req.body;
+        const user = new User({ name, age, email });
+        const savedUser = await user.save();
+        res.status(201).json(savedUser);
+        console.log(`(201) User created: ${savedUser.name}`);
+    } catch (error) {
+        errorHandler(res, error, 'Error creating user');
+    }
+};
+
+const createMultipleUsers = async (req, res) => {
+    console.log('(POST) Creating multiple users...', req.body);
+    try {
+        const usersToCreate = req.body;
+        const createdUsers = await Promise.all(usersToCreate.map(user => new User(user).save()));
+        res.status(201).json(createdUsers);
+        console.log(`(201) ${createdUsers.length} users created`);
+    } catch (error) {
+        errorHandler(res, error, 'Error creating multiple users');
+    }
+};
+
+const updateUser = async (req, res) => {
+    console.log('(PUT) Updating user...', req.body);
+    try {
+        const id = req.params.id;
+        if (!mongoose.isValidObjectId(id)) {
+            errorHandler(res, new Error('Invalid ObjectId'), undefined, 400);
+            return;
+        }
+        const user = await User.findByIdAndUpdate(id, req.body, { new: true });
         if (!user) {
             errorHandler(res, new Error('User not found'), undefined, 404);
             return;
         }
-        console.log(`(200) User updated: ${user.name}`);
         res.status(200).json(user);
-    })
-    .catch(error => {
+        console.log(`(200) User updated: ${user.name}`);
+    } catch (error) {
         errorHandler(res, error, 'Error updating user');
-    });
-}
-
-async function updateMultipleUsers(req, res) {
-    console.log('(PUT) Updating multiple users...', req.body);
-    const usersToUpdate = req.body;
-    const updates = usersToUpdate.map(user => {
-        return User.findByIdAndUpdate(user.id, user, { new: true });
-    });
-    await Promise.all(updates)
-    .then(updatedUsers => {
-        console.log(`(200) ${updatedUsers.length} users updated`);
-        res.status(200).json(updatedUsers);
-    })
-    .catch(error => {
-        errorHandler(res, error, 'Error updating multiple users');
-    });
-}
-
-async function deleteUser(req, res) {
-    console.log('(DELETE) Deleting user...', req.params.id);
-    const id = req.params.id;
-    if (!mongoose.isValidObjectId(id)) {
-        errorHandler(res, new Error('Invalid ObjectId'), undefined, 400);
-        return;
     }
-    User.findByIdAndDelete(id)
-    .then(deletedUser => {
+};
+
+const updateMultipleUsers = async (req, res) => {
+    console.log('(PUT) Updating multiple users...', req.body);
+    try {
+        const usersToUpdate = req.body;
+        const updatedUsers = await Promise.all(usersToUpdate.map(user => User.findByIdAndUpdate(user.id, user, { new: true })));
+        res.status(200).json(updatedUsers);
+        console.log(`(200) ${updatedUsers.length} users updated`);
+    } catch (error) {
+        errorHandler(res, error, 'Error updating multiple users');
+    }
+};
+
+const deleteUser = async (req, res) => {
+    console.log('(DELETE) Deleting user...', req.params.id);
+    try {
+        const id = req.params.id;
+        if (!mongoose.isValidObjectId(id)) {
+            errorHandler(res, new Error('Invalid ObjectId'), undefined, 400);
+            return;
+        }
+        const deletedUser = await User.findByIdAndDelete(id);
         if (!deletedUser) {
             errorHandler(res, new Error('User not found'), undefined, 404);
             return;
         }
+        res.status(200).json({ message: 'User deleted' });
         console.log(`(200) User deleted: ${deletedUser.name}`);
-        res.status(200).json({ message: 'Usuario eliminado' });
-    })
-    .catch(error => {
+    } catch (error) {
         errorHandler(res, error, 'Error deleting user');
-    });
-}
+    }
+};
 
-async function deleteAllUsers(req, res) {
+const deleteAllUsers = async (req, res) => {
     console.log('(DELETE) Deleting all users...');
-    User.deleteMany({})
-    .then(result => {
+    try {
+        const result = await User.deleteMany({});
+        res.status(200).json({ message: `Deleted ${result.deletedCount} users` });
         console.log(`(200) ${result.deletedCount} users deleted`);
-        res.status(200).json({ message: 'All users deleted' });
-    })
-    .catch(error => {
+    } catch (error) {
         errorHandler(res, error, 'Error deleting all users');
-    });
-}
+    }
+};
 
 export default { getUsers, createUser, createMultipleUsers, updateUser, updateMultipleUsers, deleteUser, deleteAllUsers };
