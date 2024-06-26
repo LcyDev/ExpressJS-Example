16e6bd99e217c507fac7c75be3067798a981d6bc
Lucyy_ <72966048+LcyDev@users.noreply.github.com>
2024-06-26 02:18:56 -0300

    Created object containg three dateTime formats
    Instead of exporting two separate variables, I encapsulated them in an object 'dateTimeFormats'
    Added dateTime format combining both date & time.

--- src/utils/dateTimeFormat.js
+++ src/utils/dateTimeFormat.js
@@ -1,14 +1,22 @@
+const dateTimeFormats = {
+    date: new Intl.DateTimeFormat('en', {
+        year: 'numeric',
+        month: '2-digit',
+        day: '2-digit',
+    }),
+    time: new Intl.DateTimeFormat('en', {
+        hour: '2-digit',
+        minute: '2-digit',
+        second: '2-digit',
+    }),
+    dateTime: new Intl.DateTimeFormat('en', {
+        year: 'numeric',
+        month: '2-digit',
+        day: '2-digit',
+        hour: '2-digit',
+        minute: '2-digit',
+        second: '2-digit',
+    }),
+};
 
-const dateFormat = new Intl.DateTimeFormat('en', {
-    year: 'numeric',
-    month: '2-digit',
-    day: '2-digit',
-});
-
-const timeFormat = new Intl.DateTimeFormat('en', {
-    hour: '2-digit',
-    minute: '2-digit',
-    second: '2-digit',
-});
-
-export { dateFormat, timeFormat }
+export default dateTimeFormats;
