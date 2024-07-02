b0ec104dc0e551d92e47d413e841f49cd735d060
Lucyy_ <72966048+LcyDev@users.noreply.github.com>
2024-07-02 16:09:24 -0300

    Update endpointLogger.js

--- src/middleware/endpointLogger.js
+++ src/middleware/endpointLogger.js
@@ -1,5 +1,13 @@
 import logger from '../utils/logger.js';
 
+
+/**
+ * @description This middleware logs the endpoint access details.
+ * @param {Express.Request} req - Express request object.
+ * @param {Express.Response} res - Express response object.
+ * @param {Function} next - Express next middleware function.
+ * @returns {void} - No return value.
+ */
 const endpointLogger = (req, res, next) => {
     const { method, path, query } = req;
     const queryParams = Object.keys(query).length
